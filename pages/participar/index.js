b9cca87075a9eca7e450771fdcb6a283050c5d58
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-09-19 14:52:57 -0400

    Added return and logo

--- pages/participar/index.js
+++ pages/participar/index.js
@@ -1,62 +1,11 @@
 import { useState } from 'react';
 
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../../styles/Participar.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../../styles/Participar.module.css';
 
 export default function Participar() {
     const [nombre, setNombre] = useState('');
-
-    const [departamento, setDepartamento] = useState(undefined);
-    const [municipio, setMunicipio] = useState(undefined);
-    const [edad, setEdad] = useState(undefined);
-    const [genero, setGenero] = useState(undefined);
-    const [barrio, setBarrio] = useState(undefined);
-    const [zonas, setZonas] = useState(undefined);
-
-    const [pregunta, setPregunta] = useState(1);
-
-    function mostrarPregunta() {
-        if (pregunta === 1) {
-            return (
-                <div className={styles.grid}>
-                    <div className={styles.item}>
-                        <label className={styles.label} for="departamentos"><strong>¿En qué departamento vives?</strong></label><br />
-                        <select name="departamentos">
-                            <option value="volvo">Volvo</option>
-                            <option value="saab">Saab</option>
-                            <option value="mercedes">Mercedes</option>
-                            <option value="audi">Audi</option>
-                        </select>
-                    </div>
-                    <input type="submit" value="Continuar" />
-                </div>
-            );
-        }
-        return (
-            <div className={styles.grid}>
-                <div className={styles.item}>
-                    <label for="departamentos">Departamento</label><br />
-                    <select name="departamentos">
-                        <option value="volvo">Volvo</option>
-                        <option value="saab">Saab</option>
-                        <option value="mercedes">Mercedes</option>
-                        <option value="audi">Audi</option>
-                    </select>
-                </div>
-                <div className={styles.item}>
-                    <label for="municipios">Municipio/Ciudad</label><br />
-                    <select name="municipios">
-                        <option value="volvo">Volvo</option>
-                        <option value="saab">Saab</option>
-                        <option value="mercedes">Mercedes</option>
-                        <option value="audi">Audi</option>
-                    </select>
-                </div>
-                <input type="submit" value="Continuar" />
-            </div>
-        );
-    }
 
     function handleInputChange(e) {
         setNombre(e.target.value);
@@ -73,25 +22,26 @@ export default function Participar() {
     return (
         <div className={styles.container}>
             <Head>
-                <title>Participar</title>
-                <meta name="description" content="Participa en ODS_IA y ayúdanos a comprender el territorio de manera colectiva." />
-                <meta name="keywords" content="IA, Objetivos de Desarrollo Sostenibles, UNFPA, Colombia"></meta>
+                <title>ODS-IA participar</title>
+                <meta name="description" content="Participa en ODS-IA y ayúdanos a comprender el territorio de manera colectiva." />
+                <meta name="keywords" content="IA, Objetivos de Desarrollo Sostenibles, UNFPA, Colombia, participa"></meta>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
-
+            <Link href="/"><button className={styles.button_return} role="button">&#10140;</button></Link>
+            <div className={styles.logo}>
+                ODS-IA
+            </div>
             <div className={styles.main}>
                 <h1 className={styles.title}>Participar &#9995;</h1>
                 <p className={styles.description}>Responde 6 preguntas que te permitirán saber que ODSs son los mas cercanos a tus necesidades y preocupaciones. No te demoraras más de 10 minutos y nos ayudarás a comprender el territorio de manera colectiva.</p>
                 <p className={styles.question}>¡Para empezar cuéntanos como te llamas!</p>
                 <div className={styles.item}>
                     <div className={styles.form__group}>
-                        <input type="input" className={styles.form__field} placeholder="Name" name="name" maxlength="20" value={nombre} onChange={handleInputChange} required />
-                        <label for="name" className={styles.form__label}>Nombre</label>
+                        <input type="input" className={styles.form__field} placeholder="Name" name="name" maxLength="20" value={nombre} onChange={handleInputChange} required />
+                        <label htmlFor="name" className={styles.form__label}>Nombre</label>
                     </div>
                 </div>
                 {showButton()}
-                <Link href="/"><button className={styles.button_start} role="button">Regresar</button></Link>
             </div>
         </div>
 
