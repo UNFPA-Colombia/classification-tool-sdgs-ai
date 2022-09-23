9d160681072440629ac2d7ab9f25a184384c2049
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-09-23 18:33:35 -0400

    Added api to process text to SDGs

---
+++ pages/api/procesamiento.js
@@ -0,0 +1,43 @@
+import path from 'path';
+const { spawn } = require('child_process');
+const Joi = require('joi');
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        return new Promise(function (resolve, reject) {
+            const schema = Joi.array().items(
+                Joi.string().min(10).max(1000)
+            ).required();
+
+            const { error, value } = schema.validate(req.body);
+
+            if (error) {
+                res.status(400).send(err);
+                resolve();
+            } else {
+                const libDirectory = path.join(process.cwd(), 'lib');
+                const pythonPath = libDirectory + '/virtualenv9/bin/python'; // path to python 3.9.13 virtual environment with all required libraries
+                const pythonProcess = spawn(pythonPath, [libDirectory + '/textSimilarityODS.py', ...value]);
+    
+                pythonProcess.stdout.on('data', (data) => {
+                    res.setHeader('content-type', 'application/json');
+                    res.status(200).send(data);
+                });
+    
+                pythonProcess.stderr.on('data', (data) => {
+                    console.error(`stderr: ${data}`);
+                });
+    
+                pythonProcess.on('close', (code) => {
+                    if (code !== 0) {
+                        console.log(`child process exited with code ${code}`);
+                        res.status(500).send('Internal error');
+                    }
+                    resolve();
+                });
+            }
+        });
+    } else {
+        res.status(405).send('Only POST requests allowed');
+    }
+}
