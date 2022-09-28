8a32ebba665f846a3568986a7d0bbd16342c142c
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-09-28 12:40:31 -0400

    Traducir button working

--- pages/traducir.js
+++ pages/traducir.js
@@ -4,11 +4,89 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 import TextAudioQuestion from '../components/TextAudioQuestion';
+import Loading from '../components/Loading';
 import styles from '../styles/Traducir.module.css';
 
 export default function Traducir() {
 
     const [texto, setTexto] = useState('');
+    const [estadoTraduccion, setEstadoTraduccion] = useState(0); // 0: not posted, 1: posting, 2: posted 3: error
+    const [resultadoTraduccion, setResultadoTraduccion] = useState([]);
+
+    function traducirTexto() {
+        setEstadoTraduccion(1);
+        fetch('/api/traductor', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ data: texto }),
+        }).then((response) => {
+            if (response.ok) {
+                setEstadoTraduccion(2);
+                return response.json()
+                    .then((data) => {
+                        console.log(data);
+                        setResultadoTraduccion(data);
+                    });
+            } else {
+                setEstadoTraduccion(3);
+            }
+        })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+    function showTraduccion() {
+        if (estadoTraduccion === 0) {
+            return (
+                <div className={styles.traduccion}>
+                    <p>Traducción</p>
+                    <p>Presiona el botón para traducir el texto</p>
+                </div>
+            );
+        } else if (estadoTraduccion === 1) {
+            return (
+                <div className={styles.traduciendo}>
+                    <div className={styles.logoTraduciendo}>
+                        <Loading />
+                    </div>
+                    <p>Traduciendo...</p>
+                </div>
+            );
+        } else if (estadoTraduccion === 2) {
+            return (
+                <div className={styles.traduccion}>
+                    <p>Traducción</p>
+                    {resultadoTraduccion.map((item, index) => {
+                        return <div key={index}>
+                            <p>Traduccion</p>
+                        </div>;
+                    })}
+                </div>
+            );
+        } else if (estadoTraduccion === 3) {
+            return (
+                <div className={styles.traduccion}>
+                    <p>Traducción</p>
+                    <p>Ha ocurrido un error</p>
+                </div>
+            );
+        }
+    }
+
+    function showBotonTraducir() {
+        if (estadoTraduccion === 0 || estadoTraduccion === 2 || estadoTraduccion === 3) {
+            return (
+                <button className={styles.traducirButton} role="button" onClick={traducirTexto}>&#10003; Traducir</button>
+            );
+        } else if (estadoTraduccion === 1) {
+            return (
+                <button className={styles.traducirButton} role="button" onClick={traducirTexto} disabled>&#10003; Traducir</button>
+            );
+        }
+    }
 
     return (
         <div className={styles.container}>
@@ -38,9 +116,11 @@ export default function Traducir() {
                             handleAnswer={setTexto}
                             limit={600}
                         />
+                        {showBotonTraducir()}
                     </div>
                     <div className={styles.module}>
                         <h3 className={styles.subTitle}>Traducción</h3>
+                        {showTraduccion()}
                     </div>
                 </div>
             </div>
