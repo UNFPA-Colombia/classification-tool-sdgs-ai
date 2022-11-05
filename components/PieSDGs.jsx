9622f5d3c9ee8623024bef498d702bc5bd83d927
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-11-05 19:41:57 -0400

    SDGs Pie graph

---
+++ components/PieSDGs.jsx
@@ -0,0 +1,56 @@
+import { useRef, useEffect } from "react";
+import * as d3 from "d3";
+
+function drawChart(svgRef, data, setObjetivo, objetivo) {
+
+    const width = 450,
+        height = 450,
+        margin = 40;
+
+    const radius = Math.min(width, height) / 2 - margin
+
+    let svg = d3.select(svgRef.current);
+
+    svg.selectAll("*").remove();
+
+    svg = svg.attr("viewBox", `0 0 ${width} ${height}`)
+        .append("g")
+        .attr("transform", `translate(${width / 2},${height / 2})`);
+
+    const pie = d3.pie()
+        .value(d => d.valor)
+        .padAngle(.03)
+        .sort(null);
+
+    svg
+        .selectAll('path')
+        .data(pie(data))
+        .join('path')
+        .attr('d', d3.arc()
+            .innerRadius(100)
+            .outerRadius(radius)
+        )
+        .attr('fill', d => d.data.color)
+        .on('mouseover', function (d, i) {
+            d3.select(this).transition()
+                .duration('50')
+                .attr('opacity', '.85')
+        })
+        .on('mouseout', function (d, i) {
+            d3.select(this).transition()
+                .duration('50')
+                .attr('opacity', '1');
+        })
+        .on('click', function (i, d) {
+            setObjetivo(d.data.nombre);
+        });
+}
+
+export default function PieSDGs({ objetivos, setObjetivo, objetivo }) {
+    const svg = useRef(null);
+    useEffect(() => {
+        drawChart(svg, objetivos, setObjetivo, objetivo);
+    }, [svg]);
+
+    return <svg ref={svg} />;
+};
