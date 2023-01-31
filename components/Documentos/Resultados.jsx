f0fcd7163545158cb7df22ec4aaf95b4cef9354d
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2023-01-31 18:09:06 -0500

    Added new views

--- components/Documentos/Resultados.jsx
+++ components/Documentos/Resultados.jsx
@@ -4,21 +4,35 @@ import 'bootstrap/dist/css/bootstrap.css'
 import NumberOfGroups from './NumberOfGroups';
 import GraphsView from './graphs/GraphsView';
 import WordCloudsView from './WordCloudsView';
-import ShowBarCharts from './ShowBarCharts';
-import { Button, Col, Form, Row } from 'react-bootstrap';
+import ShowBarCharts from './graphBar/ShowBarCharts';
+import GeneralCleanDataView from './GeneralData/GeneralCleanDataView';
+import GeneralDataView from './GeneralData/GeneralDataView';
+import InteractiveGraphView from './InteractiveGraph/InteractiveGraphView';
+import { Col, Form, Row } from 'react-bootstrap';
 import styles from '../../styles/DocumentosComponentes.module.css'
 
 
 function Resultados({data}) {
 
-   const bestTopicNumber = data["bestModel"]
-   const modelos = data["models"]
-   const bestModel = modelos[bestTopicNumber]
+   /* General data */
+   const dataGeneral = data["generalData"]
+
+   /* Topic modeling */
+   const bestTopicNumber = data["topicModeling"]["bestModelNumber"]
+   const modelos = data["topicModeling"]["models"]
    const [currentTopicNumber, setTopicNumber] = useState(bestTopicNumber + 2)
+
+   /* Distributions */
+   const distributions = data["distribuciones"]
+
+   /* Interactive graph */
+   const interactiveGraph = data["interactive"]
 
 
    return (
       <div>
+         <GeneralDataView data={dataGeneral} />
+         <GeneralCleanDataView data={dataGeneral} />
          <NumberOfGroups numTopics={bestTopicNumber + 2} />
          <div className={`${styles.fondoColorMoradoClaro} ${styles.center} ${styles.shadow}`} style={{padding: "1em 10em" }}>
             <Row className={styles.shadow}>
@@ -37,9 +51,10 @@ function Resultados({data}) {
                </Col>
             </Row>
          </div>
-         <GraphsView coherencia={modelos[currentTopicNumber - 2]["coherencia_c_v"]} />
+         <GraphsView coherencia={modelos[currentTopicNumber - 2]["coherencia_c_v"]} distribuciones={distributions} topicos={currentTopicNumber - 2}/>
          <WordCloudsView topicos={modelos[currentTopicNumber-2]["topicos"]} />
          <ShowBarCharts topicos={modelos[currentTopicNumber-2]["topicos"]} />
+         <InteractiveGraphView data={interactiveGraph[currentTopicNumber-2]} />
       </div>
    )
 }
