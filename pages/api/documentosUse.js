3d07ee1f3c7881b07e4ae303768ecb9ae38f0b90
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2023-02-01 11:47:04 -0500

    Added endpoint for using docs with model

---
+++ pages/api/documentosUse.js
@@ -0,0 +1,95 @@
+import nextConnect from 'next-connect';
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
+const { spawn } = require('child_process');
+const Joi = require('joi');
+
+const schema = Joi.object({
+    id: Joi.string().required(),
+    topicNumber: Joi.integer().required(),
+});
+
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'application/pdf') { // only accept pdf files
+        cb(null, true);
+    } else {
+        cb(new Error('Wrong type'));
+    }
+}
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: './lib/docsTopicModeling',
+        filename: (req, file, cb) => cb(null, file.originalname.split(".")[0].replace(/_/g, '') + '_' + file.fieldname + '_' + Date.now() + '.' + file.mimetype.split('/')[1]),
+    }),
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 1024 * 1024 * 6 // 6MB
+    },
+});
+
+const apiRoute = nextConnect({
+    onError(error, req, res) {
+        res.status(400).json({ error: `Error: ${error.message}` });
+    },
+    onNoMatch(req, res) {
+        res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+    },
+});
+
+apiRoute.use(upload.array('files'));
+
+apiRoute.post((req, res) => {
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    return new Promise(function (resolve, reject) {
+        const libDirectory = path.join(process.cwd(), 'lib');
+        let files = req.files.map((file) => {
+            return `${libDirectory}/docsTopicModeling/${file.filename}`;
+        });
+        const userID = req.body.id;
+        const topicNumber = req.body.topicNumber;
+        const pythonPath = libDirectory + '/venvTopicModeling/bin/python'; // path to python 3.9.13 virtual environment with all required libraries
+        const pythonProcess = spawn(pythonPath, [libDirectory + '/useTtopicModeling.py', files[0], libDirectory + '/dataTopicModeling/' + userID, topicNumber]);
+        console.log('Running python script use...');
+        let bufferArray= []
+        pythonProcess.stdout.on('data', (data) => {
+            bufferArray.push(data);
+        });
+
+        pythonProcess.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+
+        pythonProcess.on('close', (code) => {
+            files.forEach(element => {
+                fs.unlink(element, (err) => { });
+            });
+            if (code !== 0) {
+                console.log(`child process exited with code ${code}`);
+                res.status(500).send('Internal error');
+            }
+            else {
+                let dataBuffer = Buffer.concat(bufferArray);
+                console.log(dataBuffer.toString())
+                res.setHeader('content-type', 'application/json');
+                res.status(200).send(dataBuffer);
+            }
+            resolve();
+        });
+    });
+});
+
+export default apiRoute;
+
+// Disallow body parsing
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
